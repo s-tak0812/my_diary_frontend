f0290a88780887c3c44a8348b5a9a4b2df2a24e9
s_tak <zakk_zakuzaku_bz@yahoo.co.jp>
2025-08-12 13:39:34 +0900

    sass,router導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react()
+  ],
   server: {
     host: "0.0.0.0",
     port: 8000,
